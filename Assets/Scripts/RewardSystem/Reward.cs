9235f248d4b4a863e69808625e1669d4e2ee1466
Scott Baldwin <s4010770@glos.ac.uk>
2023-05-22 21:28:02 +0100

    reward

--- Assets/Scripts/RewardSystem/Reward.cs
+++ Assets/Scripts/RewardSystem/Reward.cs
@@ -7,17 +7,23 @@ namespace RewardSystem
 {
     public class Reward : MonoBehaviour
     {
+        [SerializeField] private float _rotationSpeed = 50f;
+        [SerializeField] private AudioClip _clip;
+        
         public static Action onCollected;
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.up, _rotationSpeed * Time.deltaTime);
+        }
 
         private void OnCollisionEnter(Collision other)
         {
-            if (other.gameObject.CompareTag("Player"))
-            {
-                // Trigger event to add the score to the HUD
-                onCollected?.Invoke();
-                Destroy(gameObject);
-                
-            }
+            if (!other.gameObject.CompareTag("Player")) return;
+            // Trigger event to add the score to the HUD
+            onCollected?.Invoke();
+            Destroy(gameObject);
+            SoundManager.Instance.PlaySound(_clip, false);
         }
     }
 }
